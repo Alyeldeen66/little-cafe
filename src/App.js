b88ec53071ddf13d8d650685c907cb0fca1202c2
Alyeldeen68 <alyeldeenkhaled3@gmail.com>
2023-02-07 15:21:07 +0200

    Added delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,177 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import littleLogo from "../src/Assets/Screenshot 2023-02-05 210245.png";
+import cartLogo from "./Assets/shopping-cart.png";
+import closeCart from "./Assets/close.png";
+import Alert from "react-bootstrap/Alert";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import {
+  addProduct,
+  decrement,
+  decrementCart,
+  deleteProduct,
+  increment,
+  incrementCart,
+} from "./Redux/productsReducer";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const products = useSelector((state) => state.products.products);
+  const cartProducts = useSelector((state) => state.products.cartProducts);
+  const totalPrice = useSelector((state) => state.products.totalPrice);
+  const dispatch = useDispatch();
+  console.log(cartProducts);
+  const handleClick = () => {
+    setShowCart(true);
+  };
+  const toggleCart = () => {
+    setShowCart(!showCart);
+  };
+  const buyProducts = (id) => {
+    dispatch(addProduct(id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-container">
+        <img src={littleLogo} />
+        {/* <h1>Welcome to the little cafe</h1> */}
+      </div>
+      <div className="shopping-cart">
+        <img onClick={handleClick} src={cartLogo} />
+      </div>
+      {showCart && (
+        <div className="shopping-list">
+          <div className="shopping-list-close">
+            <img onClick={toggleCart} src={closeCart} />
+          </div>
+          <div className="shopping-list-items-container">
+            <div className="shopping-list-items">
+              {cartProducts ? (
+                cartProducts.map((product) => (
+                  <div key={product.id} className="shopping-list-item">
+                    <Card style={{ width: "10rem", height: "14rem " }}>
+                      <Card.Img
+                        variant="top"
+                        src={product.img}
+                        style={{ height: "5rem" }}
+                      />
+                      <Card.Body>
+                        <Card.Title style={{ textAlign: "center" }}>
+                          {product.title}
+                        </Card.Title>
+                        <div className="product-count">
+                          <Button
+                            style={{
+                              backgroundColor: "#a49c8e",
+                              border: "0px",
+                            }}
+                            onClick={() => dispatch(decrementCart(product.id))}
+                          >
+                            -
+                          </Button>
+                          <p> {product.quantity}</p>
+                          <Button
+                            style={{
+                              backgroundColor: "#a49c8e",
+                              border: "0px",
+                            }}
+                            onClick={() => dispatch(incrementCart(product.id))}
+                          >
+                            +
+                          </Button>
+                        </div>
+                        <div className="product-delete">
+                          <Button
+                            style={{
+                              backgroundColor: "#a49c8e",
+                              border: "0px",
+                            }}
+                            onClick={() => dispatch(deleteProduct(product.id))}
+                          >
+                            Delete
+                          </Button>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                ))
+              ) : (
+                <h2>Empty Cart</h2>
+              )}
+              {cartProducts ? (
+                <div style={{ textAlign: "center" }}>
+                  <h4>Total Price = {totalPrice}</h4>
+                  <Button
+                    style={{
+                      backgroundColor: "#a49c8e",
+                      border: "0px",
+                    }}
+                  >
+                    Buy now
+                  </Button>
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div></div>
+          </div>
+        </div>
+      )}
+      <div className="cards-holder">
+        <div className="cards-container">
+          {products.map((product) => (
+            <div key={product.id} className="item">
+              <Card style={{ width: "18rem", border: "0px" }}>
+                <Card.Img
+                  variant="top"
+                  src={product.img}
+                  style={{ height: "14rem" }}
+                />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <div className="product-count">
+                    <Button
+                      style={{
+                        backgroundColor: "#a49c8e",
+                        border: "0px",
+                      }}
+                      onClick={() => dispatch(decrement(product.id))}
+                    >
+                      -
+                    </Button>
+                    <p> {product.quantity}</p>
+                    <Button
+                      style={{
+                        backgroundColor: "#a49c8e",
+                        border: "0px",
+                      }}
+                      onClick={() => dispatch(increment(product.id))}
+                    >
+                      +
+                    </Button>
+                  </div>
+                  <Button
+                    style={{
+                      width: "40%",
+                      backgroundColor: "#a49c8e",
+                      border: "0px",
+                    }}
+                    variant="primary"
+                    onClick={() => buyProducts(product.id)}
+                  >
+                    Buy
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Redux/productsReducer.js
@@ -0,0 +1,173 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [
+    {
+      id: 1,
+      img: "https://www.rappler.com/tachyon/2019/09/Screen-Shot-2022-05-17-at-4.42.48-PM.png",
+      title: "Red Tea",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 2,
+      img: "https://www.archanaskitchen.com/images/archanaskitchen/beverages/Green_tea_recipe.jpg",
+      title: "Green Tea",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 3,
+      img: "https://keeprecipes.com/sites/keeprecipes/files/126864_1500356770_0.jpg",
+      title: "Vanilla Tea",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 4,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTds_Kftop-_5X3iI8J_aawSLCxIIstXsfHYg&usqp=CAU",
+      title: "Cinnaman Ginger",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 5,
+      img: "https://www.feastingathome.com/wp-content/uploads/2015/12/A-long-winters-Nap-107.jpg",
+      title: "Anise",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 6,
+      img: "https://parade.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkwNTgxMjU0MTEyMzU1NDUz/hibiscus-tea-benefits-jpg.jpg",
+      title: "Hibiscus",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 7,
+      img: "https://www.nescafe.com/mena/sites/default/files/NESCAFE_RAMADAN_RECIPES_WESBITE%20PICTURES_NESCAFE_RAMADAN_RECIPES_WEBSITE_PICTURES_468X468_HONEYDATE%20%281%29.png",
+      title: "Nescafe Gold",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 8,
+      img: "https://expertreviews.b-cdn.net/sites/expertreviews/files/styles/er_main_wide/public/2020/01/untitled_design_61.png?itok=ZDh4pE3L",
+      title: "Nespresso Capsules",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+    {
+      id: 9,
+      img: "https://cdn.gourmetegypt.com/media/catalog/product/cache/2b4d21b90ad5abb98380bc0a709a4ac8/p/e/pear-imported_-fresh-source.jpg",
+      title: "Bottle Of Water",
+      count: 0,
+      quantity: 0,
+      price: 20,
+    },
+  ],
+  cartProducts: [],
+  totalPrice: 0,
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      if (state.products[action.payload - 1].quantity == 0) {
+        alert("Please select quantity !");
+        return;
+      }
+      if (state.products[action.payload - 1].count == 0) {
+        state.cartProducts.push(state.products[action.payload - 1]);
+        let index = state.cartProducts.findIndex(
+          (element) => element.id == action.payload
+        );
+        state.cartProducts[index].count++;
+      } else {
+        let index = state.cartProducts.findIndex(
+          (element) => element.id == action.payload
+        );
+        if (
+          state.products[action.payload - 1].quantity == 0 ||
+          state.products[action.payload - 1].quantity == 1
+        ) {
+          state.cartProducts[index].quantity++;
+        } else {
+          state.cartProducts[index].quantity +=
+            state.products[action.payload - 1].quantity;
+        }
+      }
+      let total = 0;
+      for (let i = 0; i < state.cartProducts.length; i++) {
+        total += state.cartProducts[i].quantity * state.cartProducts[i].price;
+      }
+      state.totalPrice = total;
+      alert(`Added ${state.products[action.payload - 1].title} Successfully`);
+    },
+    increment: (state, action) => {
+      state.products[action.payload - 1].quantity++;
+    },
+    decrement: (state, action) => {
+      if (state.products[action.payload - 1].quantity > 0) {
+        state.products[action.payload - 1].quantity--;
+      }
+    },
+    incrementCart: (state, action) => {
+      let index = state.cartProducts.findIndex(
+        (element) => element.id == action.payload
+      );
+      state.cartProducts[index].quantity++;
+      let total = 0;
+      for (let i = 0; i < state.cartProducts.length; i++) {
+        total += state.cartProducts[i].quantity * state.cartProducts[i].price;
+      }
+      state.totalPrice = total;
+    },
+    decrementCart: (state, action) => {
+      let index = state.cartProducts.findIndex(
+        (element) => element.id == action.payload
+      );
+      if (state.cartProducts[index].quantity > 1) {
+        state.cartProducts[index].quantity--;
+        let total = 0;
+        for (let i = 0; i < state.cartProducts.length; i++) {
+          total += state.cartProducts[i].quantity * state.cartProducts[i].price;
+        }
+        state.totalPrice = total;
+      }
+    },
+    deleteProduct: (state, action) => {
+      let products_to_remain = state.cartProducts.filter((product) => {
+        return product.id !== action.payload;
+      });
+      state.cartProducts = products_to_remain;
+      state.products[action.payload - 1].count = 0;
+      let total = 0;
+      for (let i = 0; i < state.cartProducts.length; i++) {
+        total += state.cartProducts[i].quantity * state.cartProducts[i].price;
+      }
+      state.totalPrice = total;
+    },
+  },
+});
+
+export const {
+  addProduct,
+  increment,
+  decrement,
+  incrementCart,
+  decrementCart,
+  deleteProduct,
+} = productsSlice.actions;
+export default productsSlice.reducer;
